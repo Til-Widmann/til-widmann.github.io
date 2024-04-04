c09873dc5b197efb8e0ebf3a312ed0fab2dc5535
Til-Widmann <64083830+Til-Widmann@users.noreply.github.com>
2024-04-04 16:00:20 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroComponent } from './hero/hero.component';
+import { HeaderComponent } from "./header/header.component";
+import { ContactComponent } from "./contact/contact.component";
+import { FeaturesComponent } from "./features/features.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, HeroComponent, ContactComponent, FeaturesComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'untitled';
+  title = 'Portfolio';
 }
+
